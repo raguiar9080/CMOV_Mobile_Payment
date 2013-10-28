a75c64a01d29373c8465e4d3ab813466d6f6a74c
Rúben Aguiar <r.aguiar9080>
2013-10-28 19:04:39 +0000

    Server First Draft

--- NodeJS_Server/server.js
+++ NodeJS_Server/server.js
@@ -1,7 +1,313 @@
 var express = require('express');
 var app = express();
-app.listen(3000);
+var	dbLib = require('./ticketsdatabase');
+var	db = new dbLib.db('ticketsdatabase.db');
+var	port = 81;	// Default
+
+//configuration
+app.configure(function() {
+	app.use(express.bodyParser());
+	app.use(function (req, res, next) {
+		res.setHeader('Server', 'MobilePayment');
+
+		return next();
+	});
+
+	app.enable('trust proxy');
+	app.disable('x-powered-by');
+
+	app.use('/', app.router);
+});
+
+var args = process.argv.splice(2),
+p = null;
+if (args.length > 0) {
+	p = parseInt(args[0]);
+	if( p )
+		port = p ;
+
+	p = null;
+}
+console.log('Listening on port: ' + port)
+app.listen(port);
+
+
+/*
+FUNCTIONS
+*/
+
+function respondToJSON(req, res, out, statusCode) {
+	var size;
+
+	out = JSON.stringify( out );
+	size = Buffer.byteLength( out, 'UTF-8' );
+
+	res.writeHead( statusCode,
+		{ 'Content-Type': 'application/json; charset=utf-8',
+		'Content-Length': size} );
+
+	res.write( out );
+	res.end();
+}
+
+/*
+ROUTES
+*/
 
 app.get('/', function(req, res) {
-	res.send('hello world');
+	res.send('MAIN PAGE');
+});
+
+//CREATE A CLIENT
+//POST PARAMS: name string, nib string, pass string, cardType string, validity string
+//RETURN JSON {name:<clientname>,id:<clientid>}
+app.post('/client/create',function (req,res) {
+	console.log('Method: ' + req.path + " [" + req.method + "]");
+	
+	var client = new dbLib.Client(req.body.name);
+	if( !req.body.name || !req.body.nib ||  !req.body.cardType || !req.body.validity ||!req.body.pass)
+	{
+		var out = {};
+		out.error = "Bad request";
+		respondToJSON( req, res, out, 400 );
+		
+	}
+	else
+	{
+		client.nib = req.body.nib;
+		client.cardType=req.body.cardType;
+		client.validity=req.body.validity;
+		client.pass = req.body.pass;
+		db.createClient(client, function(err, lastID, row) {
+			var out = {},
+			code;
+
+			if( err )
+			{
+				code = 500;
+				out.id = -1;
+				out.error = 'Impossible to add client';
+
+				console.log('Error adding client: ' + err);
+			}
+			else
+			{
+				code = 200;
+				out.id = lastID;
+				out.name=client.name;
+				console.log('Added new client: ' + lastID + ' ' + client.name);
+			}
+
+			respondToJSON( req, res, out, code );
+
+		});
+	}
+});
+
+//LOG ACCOUNT
+//POST PARAMS: name string,pass string
+//RETURN JSON {id:<clientid>}
+app.post('/client/login',function (req,res) {
+	console.log('Method: ' + req.path + " [" + req.method + "]");
+	
+	if( !req.body.name ||!req.body.pass)
+		respondToJSON( req, res, {error: 'Bad request'}, 400 );
+	else
+	{
+		var name=req.body.name,pass=req.body.pass;
+		db.login(name,pass, function(err,row) {
+			var out = {},
+			code;
+
+			if( err ) {
+				code = 500;
+				out.id = -1;
+				out.error = 'Impossible to find client. Possible DB Error.';
+
+				console.log('Error login client: ' + err);
+			}
+			else {
+				code = 200;
+				if (!row)
+				{
+					out.id = -1;
+					out.error = 'Wrong user or password';
+					
+					console.log('Fail login');
+				}
+				else
+				{
+
+					out.id=row.cid;
+					console.log('Logged in : ' +name);
+				}
+			}
+
+			respondToJSON( req, res, out, code );
+
+		});
+	}
+
+});
+
+//VALIDATE TICKET
+//POST /validate PARAMS: cid:client id, type:ticket type, bid:bus id
+//Return JSON {status:true/false}
+app.post('/validate',function(req,res){
+	if( !req.body.tid || !req.body.cid || !req.body.bid)
+		respondToJSON( req, res, {error: 'Bad request'}, 400 );
+	else
+	{
+		var cid=Number(req.body.cid);
+		var tid=Number(req.body.tid);
+		var bid=Number(req.body.bid);
+
+		db.validate(cid,tid,bid, function(err,row) {
+			var out = {},
+			code;
+
+			if( err ) {
+				code = 500;
+				out.error = 'Impossible to validate ticket';
+				out.status=false;
+				console.log('Error validating ticket: ' + err);
+			}
+			else {
+				code = 200;
+				if (!row)
+				{
+					out.error = 'Wrong PARAMS';
+					out.status=false;
+					console.log('Fail validate: ',tid,' ',cid,' ',bid);
+				}
+				else
+				{
+
+					out.status=true;
+					console.log('validate ticket: ',tid,' ',cid,' ',bid);
+				}
+			}
+
+			respondToJSON( req, res, out, code );
+
+		});
+	}
+});
+
+// LIST TICKETS
+// GET /list/:clientID PARAMS clientID: numeric client ID
+//returns {t1:21,t2:32,t3:43}
+app.post('/listTickets', function (req, res) {
+	if( !req.body.cid)
+		respondToJSON( req, res, {error: 'Bad request'}, 400 );
+	else
+	{
+		var cid=Number(req.body.cid);
+
+		db.listTickets(cid, function(err,row) {
+			var out = {},
+			code;
+
+			if( err ) {
+				code = 500;
+				out.error = 'Impossible to list tickest';
+				out.status=false;
+				console.log('Error listing tickets: ' + err);
+			}
+			else {
+				code = 200;
+				if (!row)
+				{
+					out.error = 'Wrong PARAMS';
+					out.status=false;
+					console.log('Fail listing: ',cid);
+				}
+				else
+				{
+					out.status=row;
+					console.log('listing ticket: ',cid);
+				}
+			}
+
+			respondToJSON( req, res, out, code );
+
+		});
+	}
+});
+
+// BUY TICKETS
+// POST /buy PARAMS: cid:client id, t1:nr de t1s, t2:nr de t2s,t3:nr de t3s
+// returns {t1:21,t2:32,t3:43}
+app.post('/buyTickets', function (req, res) {
+
+	var cid = Number(req.body.cid),
+	t1=Number(req.body.t1),
+	t2=Number(req.body.t2),
+	t3=Number(req.body.t3);
+	
+	if( !cid ||!t1||!t2||!t3)
+		respondToJSON( req, res, {error: 'Bad request'}, 400 );
+
+	else {
+		db.buyTickets(cid,t1,t2,t3, function(err,out) {
+			var code;
+
+			if( err ) {
+				code = 500;
+				out.error = 'Impossible to buys tickets for client';
+				console.log('Error listing tickets: ' + err);
+			}
+			else {
+				code = 200;
+				if (!out)
+				{
+					out.error = 'Wrong PARAMS';
+					console.log('Fail buy tickets: ',cid);
+				}
+				else
+				{
+					console.log('client tickets after buy: ',cid, ' ',JSON.stringify( out ));
+				}
+			}
+
+			respondToJSON( req, res, out, code );
+
+		});
+		
+	}
+});
+
+//verificar bilhetes validados num terminal
+//GET /validated/:busId PARAMS busId=id do terminal de validacao
+//returns json array client IDs [1,2,4]
+app.post("/getValidatedTickets",function(req,res){
+	var bid=Number(req.body.bid);
+	if (!bid)
+		respondToJSON( req, res, {error: 'Bad request'}, 400 );
+	else {
+		db.getValidatedTickets(bid,function(rows){
+			var code = 200;
+			var out={};
+			if (!rows)
+			{
+				out.error = 'Something went wrong getting validated';
+				console.log('Fail get validated tickets: ',bid);
+			}
+			else
+			{
+				out.list=rows;
+				console.log('validated clients: ',bid, ' ',JSON.stringify( out ));
+			}
+			
+
+			respondToJSON( req, res, out, code );
+		});
+	}
+});
+
+app.all('*', function (req, res) {
+
+	console.log('Pedido não encontrado: ' + req.path + " [" + req.method + "]");
+
+	respondToJSON( req, res, { error: 'Página não encontrada'}, 404 );
 });
---
+++ NodeJS_Server/ticketsdatabase.js
@@ -0,0 +1,263 @@
+var sqlite3 = require("sqlite3").verbose();
+var moment = require('moment');
+var fs = require("fs");
+
+ticketConn = undefined;
+
+
+module.exports.db=sqliteDB;
+module.exports.Client=Client;
+
+/*
+ *	CLIENT CLASS
+ */ 
+ 
+ function Client(name){
+ 	this.id=null;
+ 	this.name=name;
+ 	this.tickets=[];
+ 	this.nib=null;
+ 	this.cardType=null;
+ 	this.validity=null;
+ }
+
+/*
+ *	BUS CLASS
+ */ 
+ 
+ function Bus(name){
+ 	this.id=null;
+ 	this.description=description;
+ }
+
+/*
+ *	TICKET CLASS
+ */ 
+ 
+ function Ticket(name){
+ 	this.id=null;
+ 	this.type=null;
+ 	this.duration=null;
+ }
+
+ //get time
+ function timeNow()
+ {
+ 	return ( new Date() / 1000 ) | 0 ;
+ }
+
+ function timestamp(){
+ 	return moment().format("YYYY-MM-DDTHH:mm:ss");
+ }
+
+/*
+ *	DATABASE
+ */
+ 
+ function sqliteDB(file) 
+ {
+
+ 	console.log("Opening Database:"+file);
+ 	var exists = fs.existsSync(file);
+
+ 	if( !exists )
+ 	{
+ 		console.log("File not found. Creating new Database file: " + file );
+ 		fs.openSync(file, "w");
+ 	}
+
+ 	ticketConn = new sqlite3.Database(file,function() {
+ 		ticketConn.run('PRAGMA foreign_keys=on');
+ 	});
+
+ 	if( !exists )
+ 	{
+		// Serialize forces the order of the operations, nothing is parallel
+		ticketConn.serialize(function(){
+
+			ticketConn
+			.run("BEGIN;")
+
+				// Create tables
+				.run("CREATE TABLE clients (cid INTEGER PRIMARY KEY, name TEXT, nib TEXT NOT NULL, cardType TEXT NOT NULL, validity TEXT NOT NULL, pass TEXT NOT NULL, UNIQUE(name));")
+				.run("CREATE TABLE tickets (tid INTEGER PRIMARY KEY, type TEXT, duration INTEGER NOT NULL);")
+				.run("CREATE TABLE buses (bid INTEGER PRIMARY KEY, description TEXT);")
+
+				.run("CREATE TABLE transactions (id INTEGER PRIMARY KEY, cid REFERENCES clients(cid), tid REFERENCES tickets(tid), bid REFERENCES buses(bid), dateValidated TEXT, dateBought TEXT NOT NULL);")
+				
+				// Insert first data
+				.run("INSERT INTO clients (name, nib, cardType, validity, pass) VALUES ('ADMIN', 'NIB', 'CARDTYPE', 'VALIDITY', 'PASWORD') , ('Ruben', 'nib123', 'card123', 'validity123', 'passwordencrypted');")
+				.run("INSERT INTO tickets (type, duration) VALUES ('T1', 15), ('T2', 30), ('T3', 60);")
+				.run("INSERT INTO buses (description) VALUES ('Porto-Gaia'), ('Lisboa-Porto');")
+				.run("INSERT INTO transactions (cid, tid, dateBought) VALUES (1, 1, 'DATE');")
+				.run("COMMIT;");
+			});
+}
+}
+
+sqliteDB.prototype.createClient=function(client, callback)
+{
+	console.log("adding client to db ", client.name);
+	if( typeof callback !== 'function')
+		throw new Error('Callback is not a function');
+	ticketConn.run("INSERT INTO clients (name, nib, cardType, validity,pass) VALUES (?, ?, ?, ?, ?);",
+		[client.name, client.nib, client.cardType, client.validity, client.pass],
+		function(err){
+			callback(err, this.lastID, this.changes);
+		});
+}
+
+sqliteDB.prototype.login=function(name, pass, callback)
+{
+	console.log("login client: ", name);	
+	if( typeof callback !== 'function')
+		throw new Error('Callback is not a function');
+	ticketConn.all("SELECT * FROM clients WHERE name=?",
+		[name],
+		function(err, row) {
+			if( row && row.length > 0 )
+			{
+				row=row[0];
+				if (row.pass == pass)callback(err,row );
+				else callback(err,null);
+			}
+			else
+				callback(err,null );
+		});
+}
+
+sqliteDB.prototype.validate=function(cid, tid, bid, callback)
+{
+	console.log("Validating ticket:_" + tid + "_; client:_" + cid + "_");
+	console.log(typeof(tid) + typeof(cid));
+	if ( typeof callback !== 'function')
+		throw new Error('Callback is not a function');
+	ticketConn.get("SELECT * FROM transactions WHERE tid = ? AND cid = ? AND bid IS NULL",
+		[tid, cid],
+		function (err, row) {
+			if (row)
+			{
+				var date=timestamp();
+				var transaction_id=row.id;
+				ticketConn.run("UPDATE transactions SET dateValidated=?, bid=? WHERE id=?",[date,bid,transaction_id],
+					function(err)
+					{
+						if (!err)console.log("Sucess on Validation",this.changes);
+						else console.log("DB error on Validation");
+						callback(err,this.changes);
+					});
+			}
+			else
+				callback(err,null);
+		});
+}
+
+sqliteDB.prototype.listTickets=function(clientID,callback)
+{
+	console.log("list tickets for ",clientID);
+	if ( typeof callback !== 'function')
+		throw new Error('Callback is not a function');
+
+	ticketConn.all("SELECT transactions.tid, type, duration FROM transactions, tickets WHERE transactions.tid = tickets.tid AND transactions.cid = ? AND transactions.bid IS NULL",
+		[clientID],
+		function (err,row) { //eachfunction
+			console.log(err + " --- " + row);
+			if( row && row.length > 0 )
+			{
+				callback(err,row);
+			}
+			else
+				callback(err,null);
+		});	
+}
+
+sqliteDB.prototype.buyTickets=function(clientID,t1,t2,t3,callback)
+{
+	console.log("buy tickets for ",clientID);
+	if ( typeof callback !== 'function')
+		throw new Error('Callback is not a function');	
+	var count,resto;
+	resto=t3%10;
+	count=t3-resto;
+	t3+=count/10;
+	count=resto+t2;
+	resto=count%10;
+	count=count-resto;
+	t2+=count/10;
+	count=resto+t1;
+	resto=count%10;
+	count=count-resto;
+	t1+=count/10;
+	var ts=timestamp();
+	
+	var out={};out.t1=t1;out.t2=t2;out.t3=t3;
+	console.log("tickets + bonus: ",t1," ",t2," ",t3);
+	ticketConn.parallelize(function(){
+		for (var i=0;i<t3;i++)
+		{
+			ticketConn.run("INSERT INTO transactions (cid,tid, dateBought) VALUES (?,3,?);",[clientID,ts]);
+		}
+		for (var j=0;j<t2;j++)
+		{
+			ticketConn.run("INSERT INTO transactions (cid,tid, dateBought) VALUES (?,2,?);",[clientID,ts]);
+
+		}
+		for (var k=0;k<t1;k++)
+		{
+			ticketConn.run("INSERT INTO transactions (cid,tid, dateBought) VALUES (?,1,?);",[clientID,ts]);
+
+		}
+	});
+	callback(null,out);
+}
+
+sqliteDB.prototype.getValidatedTickets=function(busId,callback)
+{
+	if ( typeof callback !== 'function')
+		throw new Error('Callback is not a function');	
+	
+	console.log("get validated for ",busId);
+	var out=[];
+	var time1=moment().subtract('minutes',15).format("YYYY-MM-DDTHH:mm:ss");
+	var time2=moment().subtract('minutes',30).format("YYYY-MM-DDTHH:mm:ss");
+	var time3=moment().subtract('minutes',45).format("YYYY-MM-DDTHH:mm:ss");
+	ticketConn.serialize(function(){
+		ticketConn.all("SELECT cid,dateValidated FROM tickets WHERE type=3 AND busID=? AND dateValidated>?",[busId,time3],
+		function (err,row) {
+			if (row && !err)
+			{
+				console.log("validated t3 ",JSON.stringify(row));
+				for (var i=0;i<row.length;i++)
+				{
+					out.push(row[i].cid);
+				}
+			}
+			else
+				console.log("error get validated t3: ",err);
+		});
+		ticketConn.all("SELECT cid,dateValidated FROM tickets WHERE type=2 AND busID=? AND dateValidated>?",[busId,time2],
+		function (err,row) {
+			if (row && !err)
+			{
+				console.log("validated t2 ",JSON.stringify(row));
+				for (var i=0;i<row.length;i++)
+					out.push(row[i].cid);
+			}
+			else
+				console.log("error get validated t2: ",err);
+		});
+		ticketConn.all("SELECT cid,dateValidated FROM tickets WHERE type=1 AND busID=? AND dateValidated>?",[busId,time1],
+		function (err,row) {
+			if (row && !err)
+			{
+			
+				console.log("validated t1",JSON.stringify(row));
+				for (var i=0;i<row.length;i++)
+					out.push(row[i].cid);
+			}
+			else
+				console.log("error get validated t1: ",err);
+			callback(out);
+		});
+	});
+}
