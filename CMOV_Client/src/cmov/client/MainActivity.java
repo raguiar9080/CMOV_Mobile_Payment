626e2947de105143cad2d3aabeac086d17a275de
Rúben Aguiar <r.aguiar9080>
2013-10-30 15:25:38 +0000

    Common Network Added on Android Client Project.
    General Network Template added. Common.

--- CMOV_Client/src/cmov/client/MainActivity.java
+++ CMOV_Client/src/cmov/client/MainActivity.java
@@ -1,8 +1,18 @@
 package cmov.client;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
+
+import common.Network;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +20,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//new CreateClient().execute();
 	}
 
 	@Override
@@ -18,5 +29,46 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private class CreateClient extends AsyncTask<String, Void,  JSONObject> {
+		private ArrayList<NameValuePair> elems = new ArrayList<NameValuePair>();
+		@Override
+		protected void onPreExecute() {
+			elems.add(new BasicNameValuePair("name","Ruben"));
+			elems.add(new BasicNameValuePair("pass","passwordencrypted"));		
+			super.onPreExecute();
+		}
+		@Override
+		protected JSONObject doInBackground(String... params) {
+			Network connection = new Network("http://10.13.37.34:81/client/create", "POST", elems);
+			connection.run();
+			return connection.getResultObject();
+		}
+	     protected void onPostExecute(JSONObject result) {
+	         Toast.makeText(getApplicationContext(),result.toString() , Toast.LENGTH_LONG).show();
+	     }
+	 }
+	
+	private class LoginClient extends AsyncTask<String, Void,  JSONObject> {
+		private ArrayList<NameValuePair> elems = new ArrayList<NameValuePair>();
+		@Override
+		protected void onPreExecute() {
+			elems.add(new BasicNameValuePair("name","Ruben"));		
+			elems.add(new BasicNameValuePair("nib","nib123"));		
+			elems.add(new BasicNameValuePair("cardType","cardVisa"));		
+			elems.add(new BasicNameValuePair("validity","Valid2013"));		
+			elems.add(new BasicNameValuePair("pass","password"));				
+			super.onPreExecute();
+		}
+		@Override
+		protected JSONObject doInBackground(String... params) {
+			Network connection = new Network("http://10.13.37.34:81/client/login", "POST", elems);
+			connection.run();
+			return connection.getResultObject();
+		}
+	     protected void onPostExecute(JSONObject result) {
+	         Toast.makeText(getApplicationContext(),result.toString() , Toast.LENGTH_LONG).show();
+	     }
+	 }
 
 }
