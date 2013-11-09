b3c66d6f759913799e95a04c87ebcffbec4ebb7e
Vítor Santos <vhsantos91@gmail.com>
2013-11-09 00:17:05 +0000

    Inspector Possible Final Added
    Needs validation testing

---
+++ Inspector/src/com/CMOV/inspector/CaptureActivity.java
@@ -0,0 +1,99 @@
+package com.CMOV.inspector;
+
+import java.util.EnumSet;
+import java.util.Set;
+import com.google.zxing.Result;
+import com.google.zxing.ResultMetadataType;
+import com.jwetherell.quick_response_code.result.ResultHandler;
+import com.jwetherell.quick_response_code.result.ResultHandlerFactory;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.TextView;
+import com.jwetherell.quick_response_code.DecoderActivity;
+
+public class CaptureActivity  extends DecoderActivity {
+	
+	private static final String TAG = CaptureActivity.class.getSimpleName();
+    @SuppressWarnings("unused")
+	private static final Set<ResultMetadataType> DISPLAYABLE_METADATA_TYPES = EnumSet.of(ResultMetadataType.ISSUE_NUMBER, ResultMetadataType.SUGGESTED_PRICE,
+            ResultMetadataType.ERROR_CORRECTION_LEVEL, ResultMetadataType.POSSIBLE_COUNTRY);
+
+    private TextView statusView = null;
+    private View resultView = null;
+    private boolean inScanMode = false;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.capture);
+        Log.v(TAG, "onCreate()");
+
+        resultView = findViewById(R.id.result_view);
+        statusView = (TextView) findViewById(R.id.status_view);
+
+        inScanMode = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "onPause()");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (inScanMode)
+                finish();
+            else
+                onResume();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void handleDecode(Result rawResult, Bitmap barcode) {
+        drawResultPoints(barcode, rawResult);
+
+        ResultHandler resultHandler = ResultHandlerFactory.makeResultHandler(this, rawResult);
+        handleDecodeInternally(rawResult, resultHandler, barcode);
+    }
+
+    protected void showScanner() {
+        inScanMode = true;
+        resultView.setVisibility(View.GONE);
+        statusView.setText(R.string.msg_default_status);
+        statusView.setVisibility(View.VISIBLE);
+        viewfinderView.setVisibility(View.VISIBLE);
+    }
+
+    // Put up our own UI for how to handle the decodBarcodeFormated contents.
+    private void handleDecodeInternally(Result rawResult, ResultHandler resultHandler, Bitmap barcode) {
+        onPause();
+        Intent intent=new Intent();
+        intent.putExtra(MainActivity.CONTENT_QR, resultHandler.getDisplayContents());
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+        
+    }
+
+}
---
+++ Inspector/src/com/CMOV/inspector/ListTicketActivity.java
@@ -0,0 +1,9 @@
+package com.CMOV.inspector;
+
+import android.app.Activity;
+
+
+//Mostrar todos os bilhetes do autocarro com id's e resultado de validação
+public class ListTicketActivity extends Activity{
+	
+}
