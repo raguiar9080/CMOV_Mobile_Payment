8adabe3b828b3ee220bb2d94fce789dd19e4322c
Vítor Santos <vhsantos91@gmail.com>
2013-11-11 10:16:46 +0000

    Deleted File

--- Inspector/src/com/CMOV/inspector/ListTicketActivity.java
+++
@@ -1,9 +0,0 @@
-package com.CMOV.inspector;
-
-import android.app.Activity;
-
-
-//Mostrar todos os bilhetes do autocarro com id's e resultado de validação
-public class ListTicketActivity extends Activity{
-	
-}
